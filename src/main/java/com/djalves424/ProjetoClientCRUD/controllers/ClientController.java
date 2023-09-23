154dc064e20c369c0861b10a2f84ff68c00ae4d5
Diego Alves <djalves424@gmail.com>
2023-09-22 22:55:00 -0300

    POST new ClientController

--- src/main/java/com/djalves424/ProjetoClientCRUD/controllers/ClientController.java
+++ src/main/java/com/djalves424/ProjetoClientCRUD/controllers/ClientController.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -28,7 +31,9 @@ public class ClientController {
     }
 
     @PostMapping
-    public ClientDTO insert(@RequestBody ClientDTO dto) {
-        return service.insert(dto);
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        dto =  service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
