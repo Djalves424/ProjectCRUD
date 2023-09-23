822f07e259263d0d7e7bb142ce81ff32bf46f64e
Diego Alves <djalves424@gmail.com>
2023-09-22 23:36:42 -0300

    Adding lombok to your pom file

--- src/main/java/com/djalves424/ProjetoClientCRUD/entities/Client.java
+++ src/main/java/com/djalves424/ProjetoClientCRUD/entities/Client.java
@@ -1,10 +1,17 @@
 package com.djalves424.ProjetoClientCRUD.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
 @Table(name = "tb_client")
 public class Client {
 
@@ -21,62 +28,5 @@ public class Client {
 
     public Client() {
 
-    }
-
-    public Client(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
-        this.id = id;
-        this.name = name;
-        this.cpf = cpf;
-        this.income = income;
-        this.birthDate = birthDate;
-        this.children = children;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Double getIncome() {
-        return income;
-    }
-
-    public void setIncome(Double income) {
-        this.income = income;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Integer getChildren() {
-        return children;
-    }
-
-    public void setChildren(Integer children) {
-        this.children = children;
     }
 }
